79bf09815acf15c4d7720ed3c158ae3df97e689b
infernumx <spaghetti90314@gmail.com>
2021-06-22 07:21:59 -0700

    Allow for selection of dump file

--- analysis.py
+++ analysis.py
@@ -1,14 +1,12 @@
+import easygui
 import pandas as pd
 import numpy as np
 import streamlit as st
 import altair as alt
-import glob
-import os
 from bandwidth_monitor import NetworkData
 import datetime
 
-
-filename = max(glob.glob('dumps/*'), key=os.path.getctime)
+filename = easygui.fileopenbox()
 data_by_hour = [[0, 0]]
 
 
@@ -23,8 +21,10 @@ def process_dumpfile(filename):
     with open(filename) as f:
         for line in f:
             network_data = eval(line)
-            delta = datetime.timedelta(seconds=network_data.time.second,
-                                       microseconds=network_data.time.microsecond)
+            delta = datetime.timedelta(
+                seconds=network_data.time.second,
+                microseconds=network_data.time.microsecond,
+            )
             if not base:
                 base = network_data
                 future = base.time + (datetime.timedelta(0, hours=1) - delta)
@@ -38,33 +38,29 @@ def process_dumpfile(filename):
 
     return sent, recv
 
-def create_line_graph(data, y_axis):
-    df = pd.DataFrame({
-        y_axis: data,
-        'Hours': [i for i in range(len(data))]
-    })
 
-    return alt.Chart(df).mark_line().encode(
-        x='Hours',
-        y=y_axis
-    )
+def create_line_graph(data, y_axis):
+    df = pd.DataFrame({y_axis: data, "Hours": [i for i in range(len(data))]})
+
+    return alt.Chart(df).mark_line().encode(x="Hours", y=y_axis)
+
 
 sent, recv = process_dumpfile(filename)
 data_by_hour = list(zip(sent, recv))
 
-st.title('Bandwidth Monitor')
+st.title("Bandwidth Monitor")
 
-data = [{'Sent': sent,
-        'Received': received}
-        for sent, received in data_by_hour]
+data = [{"Sent": sent, "Received": received} for sent, received in data_by_hour]
 
 data_length = len(data_by_hour)
 fill = len(str(data_length))
 
-df = pd.DataFrame(data, index=[f'Hour {str(i).zfill(fill)}' for i in range(data_length)])
+df = pd.DataFrame(
+    data, index=[f"Hour {str(i).zfill(fill)}" for i in range(data_length)]
+)
 st.line_chart(df)
 
-sent_chart = create_line_graph(sent, 'Sent data in MB')
-recv_chart = create_line_graph(recv, 'Received data in MB')
+sent_chart = create_line_graph(sent, "Sent data in MB")
+recv_chart = create_line_graph(recv, "Received data in MB")
 
 st.altair_chart(sent_chart | recv_chart)
